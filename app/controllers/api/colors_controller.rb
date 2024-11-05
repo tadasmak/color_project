a27723e17db8468913335e34ead537b2bd8e90a3
Tadas Maksimavicius <tadasmak@gmail.com>
2024-11-05 16:38:57 +0200

    Generate colors API controller

---
+++ app/controllers/api/colors_controller.rb
@@ -0,0 +1,10 @@
+class Api::ColorsController < ApplicationController
+  # TODO: create an API server to have these APIs:
+  # GET /colors/complementary which would return a complementary color combination by param[:color]
+  # GET /colors/triadic which would return a triadic color combination by param[:color]
+  # GET /colors/tetradic which would return a tetradic color combination by param[:color]
+  # GET /colors/analogous which would return a analogous color combination by param[:color]
+  # GET /colors/split_complementary which would return a split_complementary color combination by param[:color]
+
+  
+end
