143565487a68e3c105a3b669c44f20373e89887d
Tadas Maksimavicius <tadasmak@gmail.com>
2024-11-06 16:45:08 +0200

    Add rgb or hex forming methods and fix API's returning result

--- app/helpers/api/colors_helper.rb
+++ app/helpers/api/colors_helper.rb
@@ -5,7 +5,9 @@ module Api::ColorsHelper
 
     rgb_array = color_object.rgb_array
 
-    case method_name
+    result = [color]
+
+    calculated_colors = case method_name
     when 'complementary'
       complementary(rgb_array)
     when 'triadic'
@@ -18,6 +20,17 @@ module Api::ColorsHelper
       split_complementary
     end
 
+    if color_type == 'hex'
+      calculated_colors.each do |c|
+        result.push(form_hex(c))
+      end
+    elsif color_type == 'rgb'
+      calculated_colors.each do |c|
+        result.push(form_rgb(c))
+      end
+    end
+
+    result
   end
 
   def determine_color_type(color)
@@ -28,13 +41,21 @@ module Api::ColorsHelper
 
     nil
   end
+
+  def form_rgb(rgb_values)
+    return "rgb(#{rgb_values[:r]}, #{rgb_values[:g]}, #{rgb_values[:b]})"
+  end
+
+  def form_hex(rgb_values)
+    return "##{rgb_values[:r].to_s(16)}#{rgb_values[:g].to_s(16)}#{rgb_values[:b].to_s(16)}"
+  end
     
   def complementary(rgb_values)
     r = 255 - rgb_values[:r]
     g = 255 - rgb_values[:g]
     b = 255 - rgb_values[:b]
 
-    {r:, g:, b:}
+    [{r:, g:, b:}]
   end
 
   def triadic; end
