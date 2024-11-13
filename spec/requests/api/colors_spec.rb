3638126e99cf7f8ec963bef6acaf336e43049d65
Tadas Maksimavicius <tadasmak@gmail.com>
2024-11-13 18:54:13 +0200

    Install "rspec" and spec out color combinations API methods

--- spec/requests/api/colors_spec.rb
+++ spec/requests/api/colors_spec.rb
@@ -2,21 +2,136 @@ require 'swagger_helper'
 
 RSpec.describe 'api/colors', type: :request do
 
-  path '/api/colors/{method_name}' do
-    # You'll want to customize the parameter types...
-    parameter name: 'method_name', in: :path, type: :string, description: 'method_name'
+  path '/api/colors/complementary' do
+    # Defined required query parameter 'color'
+    parameter name: 'color', in: :query, type: :string, required: true,
+              description: 'Color value in rgb or hex format (e.g. rgb(120, 240, 15), #78f00f)'
 
-    get('show color') do
+    get('Get complementary colors combination') do
       response(200, 'successful') do
-        let(:method_name) { '123' }
+        schema type: :array,
+               items: { type: :string, description: 'Complementary colors combination'}
+        
+        let(:color) { '#78a812' }
 
-        after do |example|
-          example.metadata[:response][:content] = {
-            'application/json' => {
-              example: JSON.parse(response.body, symbolize_names: true)
-            }
-          }
+        run_test! do |response|
+          colors = JSON.parse(response.body)
+          expect(colors.length).to eq(2)
         end
+      end
+
+      response(400, 'invalid parameter') do
+        schema type: :object, properties: { error: { type: :string } }, required: ['error']
+
+        let(:color) { 'invalid_color_format' }
+        run_test!
+      end
+    end
+  end
+
+  path '/api/colors/triadic' do
+    # Defined required query parameter 'color'
+    parameter name: 'color', in: :query, type: :string, required: true,
+              description: 'Color value in rgb or hex format (e.g. rgb(120, 240, 15), #78f00f)'
+
+    get('Get triadic colors combination') do
+      response(200, 'successful') do
+        schema type: :array,
+               items: { type: :string, description: 'Triadic colors combination'}
+        
+        let(:color) { '#78a812' }
+
+        run_test! do |response|
+          colors = JSON.parse(response.body)
+          expect(colors.length).to eq(3)
+        end
+      end
+
+      response(400, 'invalid parameter') do
+        schema type: :object, properties: { error: { type: :string } }, required: ['error']
+
+        let(:color) { 'invalid_color_format' }
+        run_test!
+      end
+    end
+  end
+
+  path '/api/colors/tetradic' do
+    # Defined required query parameter 'color'
+    parameter name: 'color', in: :query, type: :string, required: true,
+              description: 'Color value in rgb or hex format (e.g. rgb(120, 240, 15), #78f00f)'
+
+    get('Get tetradic colors combination') do
+      response(200, 'successful') do
+        schema type: :array,
+               items: { type: :string, description: 'Tetradic colors combination'}
+        
+        let(:color) { '#78a812' }
+
+        run_test! do |response|
+          colors = JSON.parse(response.body)
+          expect(colors.length).to eq(4)
+        end
+      end
+
+      response(400, 'invalid parameter') do
+        schema type: :object, properties: { error: { type: :string } }, required: ['error']
+
+        let(:color) { 'invalid_color_format' }
+        run_test!
+      end
+    end
+  end
+
+  path '/api/colors/analogous' do
+    # Defined required query parameter 'color'
+    parameter name: 'color', in: :query, type: :string, required: true,
+              description: 'Color value in rgb or hex format (e.g. rgb(120, 240, 15), #78f00f)'
+
+    get('Get analogous colors combination') do
+      response(200, 'successful') do
+        schema type: :array,
+               items: { type: :string, description: 'Analogous colors combination'}
+        
+        let(:color) { '#78a812' }
+
+        run_test! do |response|
+          colors = JSON.parse(response.body)
+          expect(colors.length).to eq(3)
+        end
+      end
+
+      response(400, 'invalid parameter') do
+        schema type: :object, properties: { error: { type: :string } }, required: ['error']
+
+        let(:color) { 'invalid_color_format' }
+        run_test!
+      end
+    end
+  end
+
+  path '/api/colors/split_complementary' do
+    # Defined required query parameter 'color'
+    parameter name: 'color', in: :query, type: :string, required: true,
+              description: 'Color value in rgb or hex format (e.g. rgb(120, 240, 15), #78f00f)'
+
+    get('Get split complementary colors combination') do
+      response(200, 'successful') do
+        schema type: :array,
+               items: { type: :string, description: 'Split complementary colors combination'}
+        
+        let(:color) { '#78a812' }
+
+        run_test! do |response|
+          colors = JSON.parse(response.body)
+          expect(colors.length).to eq(3)
+        end
+      end
+
+      response(400, 'invalid parameter') do
+        schema type: :object, properties: { error: { type: :string } }, required: ['error']
+
+        let(:color) { 'invalid_color_format' }
         run_test!
       end
     end
