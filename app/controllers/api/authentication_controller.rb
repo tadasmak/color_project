e6807095a8dfad1aa49eea31b5fb4f2d1b4d0eba
Tadas Maksimavicius <tadasmak@gmail.com>
2024-11-21 17:10:55 +0200

    Set up Authentication controller to have login action with JWT token generation

--- app/controllers/api/authentication_controller.rb
+++ app/controllers/api/authentication_controller.rb
@@ -1,2 +1,24 @@
 class Api::AuthenticationController < ApplicationController
+  def login
+    email = params[:email]
+    password = params[:password]
+
+    if valid_credentials?(email, password)
+      token = generate_token(email:)
+      render json: { token: token }, status: :ok
+    else
+      render json: { error: 'Invalid email or password' }, status: :unauthorized
+    end
+  end
+  
+  private
+
+  def valid_credentials?
+    email == ENV['APP_USER'] && password == ENV['APP_PASSWORD']
+  end
+
+  def generate_token(payload)
+    payload[:expire_time] = 24.hours.from_now.to_i
+    JWT.encode(payload, Rails.application.secrets.secret_key_base)
+  end
 end
