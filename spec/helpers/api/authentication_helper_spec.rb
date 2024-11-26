f944086c042c10bf1a6c025f35d2839f40cded3f
Tadas Maksimavicius <tadasmak@gmail.com>
2024-11-26 16:47:27 +0200

    Add color combination tests using rgb format

--- spec/helpers/api/authentication_helper_spec.rb
+++ spec/helpers/api/authentication_helper_spec.rb
@@ -10,6 +10,6 @@ require 'rails_helper'
 #     end
 #   end
 # end
-RSpec.describe Api::AuthenticationHelper, type: :helper do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
+# RSpec.describe Api::AuthenticationHelper, type: :helper do
+#   pending "add some examples to (or delete) #{__FILE__}"
+# end
--- spec/integration/colors_spec.rb
+++ spec/integration/colors_spec.rb
@@ -17,11 +17,21 @@ describe 'api/colors', type: :request do
       response(200, 'successful') do
         schema type: :array,
                items: { type: :string, description: 'Complementary colors combination'}
+        
+        context 'when color is hex format' do
+          let(:color) { '#78a812' }
+          run_test! do |response|
+            colors = JSON.parse(response.body)
+            expect(colors).to match_array(['#78a812', '#4212a8'])
+          end
+        end
 
-        let(:color) { '#78a812' }
-        run_test! do |response|
-          colors = JSON.parse(response.body)
-          expect(colors).to match_array(['#78a812', '#4212a8'])
+        context 'when color is rgb format' do
+          let(:color) { 'rgb(100, 71, 228)' }
+          run_test! do |response|
+            colors = JSON.parse(response.body)
+            expect(colors).to match_array(['rgb(100, 71, 228)', 'rgb(199, 228, 71)'])
+          end
         end
       end
 
@@ -45,11 +55,21 @@ describe 'api/colors', type: :request do
         schema type: :array,
                items: { type: :string, description: 'Triadic colors combination'}
         
-        let(:color) { '#78a812' }
+        context 'when color is hex format' do
+          let(:color) { '#78a812' }
 
-        run_test! do |response|
-          colors = JSON.parse(response.body)
-          expect(colors).to match_array(['#78a812', '#1278a8', '#a81278'])
+          run_test! do |response|
+            colors = JSON.parse(response.body)
+            expect(colors).to match_array(['#78a812', '#1278a8', '#a81278'])
+          end
+        end
+
+        context 'when color is rgb format' do
+          let(:color) { 'rgb(100, 71, 228)' }
+          run_test! do |response|
+            colors = JSON.parse(response.body)
+            expect(colors).to match_array(['rgb(100, 71, 228)', 'rgb(71, 228, 100)', 'rgb(228, 100, 71)'])
+          end
         end
       end
 
@@ -73,11 +93,21 @@ describe 'api/colors', type: :request do
         schema type: :array,
                items: { type: :string, description: 'Tetradic colors combination'}
         
-        let(:color) { '#78a812' }
+        context 'when color is hex format' do
+          let(:color) { '#78a812' }
 
-        run_test! do |response|
-          colors = JSON.parse(response.body)
-          expect(colors).to match_array(['#78a812', '#12a88d', '#4212a8', '#a8122d'])
+          run_test! do |response|
+            colors = JSON.parse(response.body)
+            expect(colors).to match_array(['#78a812', '#12a88d', '#4212a8', '#a8122d'])
+          end
+        end
+
+        context 'when color is rgb format' do
+          let(:color) { 'rgb(100, 71, 228)' }
+          run_test! do |response|
+            colors = JSON.parse(response.body)
+            expect(colors).to match_array(['rgb(100, 71, 228)', 'rgb(228, 71, 120)', 'rgb(199, 228, 71)', 'rgb(71, 228, 178)'])
+          end
         end
       end
 
@@ -101,11 +131,21 @@ describe 'api/colors', type: :request do
         schema type: :array,
                items: { type: :string, description: 'Analogous colors combination'}
         
-        let(:color) { '#78a812' }
+        context 'when color is hex format' do
+          let(:color) { '#78a812' }
 
-        run_test! do |response|
-          colors = JSON.parse(response.body)
-          expect(colors).to match_array(['#78a812', '#2da812', '#a88d12'])
+          run_test! do |response|
+            colors = JSON.parse(response.body)
+            expect(colors).to match_array(['#78a812', '#2da812', '#a88d12'])
+          end
+        end
+
+        context 'when color is rgb format' do
+          let(:color) { 'rgb(100, 71, 228)' }
+          run_test! do |response|
+            colors = JSON.parse(response.body)
+            expect(colors).to match_array(['rgb(100, 71, 228)', 'rgb(178, 71, 228)', 'rgb(71, 120, 228)'])
+          end
         end
       end
 
@@ -129,11 +169,21 @@ describe 'api/colors', type: :request do
         schema type: :array,
                items: { type: :string, description: 'Split complementary colors combination'}
         
-        let(:color) { '#78a812' }
+        context 'when color is hex format' do
+          let(:color) { '#78a812' }
 
-        run_test! do |response|
-          colors = JSON.parse(response.body)
-          expect(colors).to match_array(['#78a812', '#122da8', '#8d12a8'])
+          run_test! do |response|
+            colors = JSON.parse(response.body)
+            expect(colors).to match_array(['#78a812', '#122da8', '#8d12a8'])
+          end
+        end
+
+        context 'when color is rgb format' do
+          let(:color) { 'rgb(100, 71, 228)' }
+          run_test! do |response|
+            colors = JSON.parse(response.body)
+            expect(colors).to match_array(['rgb(100, 71, 228)', 'rgb(120, 228, 71)', 'rgb(228, 178, 71)'])
+          end
         end
       end
 
