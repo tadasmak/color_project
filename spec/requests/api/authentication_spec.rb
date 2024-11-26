1dd43cf1f42d04d4bd6b94a12f72157745c81960
Tadas Maksimavicius <tadasmak@gmail.com>
2024-11-26 16:30:45 +0200

    Create spec tests for authorization when calling authorization-requiring APIs

--- spec/requests/api/authentication_spec.rb
+++ spec/requests/api/authentication_spec.rb
@@ -1,1 +1,30 @@
 require 'rails_helper'
+
+RSpec.describe 'Authentication', type: :request do
+  include AuthHelper
+
+  describe 'JWT Authentication' do
+    context 'with a valid JWT token' do
+      it 'returns a successful response' do
+        headers = { 'Authorization' => auth_headers }
+        get '/api/colors/complementary', headers: headers, params: { color: '#78a812' }
+        expect(response.status).to eq(200)
+      end
+    end
+
+    context 'without a JWT token' do
+      it 'returns an unauthorized response' do
+        get '/api/colors/complementary', params: { color: '#78a812' }
+        expect(response.status).to eq(401)
+      end
+    end
+
+    context 'with an invalid JWT token' do
+      it 'returns an unauthorized response' do
+        headers = { 'Authorization' => 'Bearer invalid_token' }
+        get '/api/colors/complementary', headers: headers, params: { color: '#78a812' }
+        expect(response.status).to eq(401)
+      end
+    end
+  end
+end
